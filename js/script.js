9163bb996b760691ae924f9be6f72d0667027a61
ayyathullahahmad <ayyathullahahmad@gmail.com>
2024-01-20 15:53:50 +0700

    adding new study case create acc

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// Get all elements with class "myImg"
+var images = document.getElementsByClassName('myImg');
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+
+// Iterate through all images and attach the onclick event
+for (var i = 0; i < images.length; i++) {
+    images[i].onclick = function() {
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        modalImg.alt = this.alt;
+        captionText.innerHTML = this.alt;
+    }
+}
+
+// When the user clicks on <span> (x), close the modal
+modal.onclick = function() {
+    img01.className += " out";
+    setTimeout(function() {
+       modal.style.display = "none";
+       img01.className = "modal-content";
+     }, 400);
+}
